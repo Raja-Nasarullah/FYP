2b3c5658d87d2102d5d0c9ad7b868d6ab7355419
Raja-Nasarullah <221049@students.au.edu.pk>
2025-09-27 16:29:23 +0500

    Commit 3
    Latest Commit

---
+++ FYP/API/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Model;
+
+namespace API.Services
+{
+    public interface IUserService
+    {
+        Task<List<User>> GetAllAsync();
+        Task<User?> GetByIdAsync(string id);
+        Task CreateAsync(User user);
+        Task UpdateAsync(string id, User user);
+        Task DeleteAsync(string id);
+    }
+}
